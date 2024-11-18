587c8d434add65455000e4ea0c6103ec43bf8cfc
Yuriy Bandura <bandurayv@gmail.com>
2024-11-18 20:11:20 +0300

    Add_Part_2

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b
-
+def check(number):
+    return number % 2 == 0
